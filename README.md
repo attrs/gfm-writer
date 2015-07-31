2f312a996f845042c03abf3edd87749e6cdc830e
joje <joje.attrs@gmail.com>
2015-07-31 18:24:31 +0900

    ..

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # GFM Writer
 
+## Installation
+```sh
+$ (sudo) npm install -g gfmw
+```
+
+### Run
+```sh
+$ gfmw filename.md
+```
+
+Or
+
+```sh
+$ gfmw
+```
+
+And Drag & Drop .md file into editor.
---
+++ bin/gfmw.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var findpath = require('nodewebkit').findpath;
+var spawn = require('child_process').spawn;
+var path = require('path');
+
+process.title = 'GFM Writer';
+
+var nwpath = findpath();
+var apppath = path.resolve(__dirname, '../');
+var argv = process.argv;
+var argc = [apppath];
+
+for(var i=2; i < argv.length; i++) {
+  argc.push(argv[i]);
+}
+
+console.log('argc', argc);
+
+spawn(nwpath, argc, {
+  cwd: process.cwd(),
+  stdio: 'inherit'
+});
